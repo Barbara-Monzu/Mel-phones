556b86f20553e3b13c7f9c0b3fea8577e4b219d5
Bárbara Monzú <monzu.barbara@gmail.com>
2021-12-31 16:42:48 +0100

    Heroku2

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,9 @@
 
 module.exports = app => {
 
-  app.use("/phones", require("./phones.routes"));
-  app.use("/reviews", require("./reviews.routes"));
-  app.use("/uploads", require("./uploads.routes"));
+  app.use("api/phones", require("./phones.routes"));
+  app.use("api/reviews", require("./reviews.routes"));
+  app.use("api/uploads", require("./uploads.routes"));
 
 
 }
